40f6320065732eb6bde4ed1cb1dafb784bbb72be
Samuel Berin <SammyJB17@gmail.com>
2020-07-12 02:54:18 -0400

    title screen start

---
+++ OutOfControl/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Button startGame;
+    public Button credits;
+    public Button gimmeCat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGame.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        });
+
+        credits.onClick.AddListener(() =>
+        {
+
+        });
+
+        gimmeCat.onClick.AddListener(() =>
+        {
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
